c7f4c6fbef254e2b5f96e7a9df9395c560cf5ea8
Patrobas Bwire <patrobasbwire@gmail.com>
2022-09-28 17:25:25 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,28 @@
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+function NavBar ()
+{
+  return (
+    <div className="navbar">
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        {" "}
+        <NavLink to="/movies">Movies</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/directors">Directors</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/actors">Actors</NavLink>
+      </li>
+    </div>
+  );
 }
 
 export default NavBar;
